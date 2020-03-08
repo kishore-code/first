a3811771f66a53ffde9c8f67aa1e3723418f4017
kishore-code <61907405+kishore-code@users.noreply.github.com>
2020-03-09 02:43:33 +0530

    Update hello.rb

--- hello.rb
+++ hello.rb
@@ -1,5 +1,5 @@
 print "How old are you ? "
 age = gets.chomp
-print "How tall are you?"
+print "How tall are you edited in web?"
 height = gets.chomp
 puts " You are #{age} year old and your height is #{height} cms"
